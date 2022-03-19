8b92823aacadd2b1184b5b988ee53aaf89beee15
Sandeep <sandyskr26@gmail.com>
2022-03-19 16:15:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About'; 
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+ 
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null); 
+    }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode ==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has been enabled", "success");
+      // document.title = "TextUtils--Dark Mode"; 
+
+      // setInterval(() => {
+      // document.title = "TextUtils is Amazing";        
+      // }, 2000);
+
+      // setInterval(() => {
+      // document.title = "Install TextUtils Now"        
+      // }, 1500);
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled", "success");
+      // document.title = "TextUtils--Light Mode";
+
+    }
+  }
+
+  return (
+    <>      
+      <Router> 
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert}/>
+        <div className="container my-3">   
+      <Switch>  
+        <Route exact path="/about">
+        <About mode={mode}/>
+        </Route>
+          <Route exact path="/">
+        <TextForm showAlert={showAlert} heading = "Enter the text - to manipulate" mode={mode}/>
+        
+        </Route>                          
+          
+        </Switch>    
+        </div>
+      </Router>    
+      
+    </>
+  ); 
+}
+ 
 export default App;
